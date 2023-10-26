ff21ecd85f969a80968251e6be873a781e618981
yongruifang <yongruifang@163.com>
2023-10-26 21:31:55 +0800

    对修改进行响应

--- README.md
+++ README.md
@@ -5,3 +5,8 @@
 # 添加Bootstrap支持
 `npm install --save bootstrap reactstrap @types/bootstrap @types/reactstrap`
 ![界面](public/ui-init.png)
+
+# 条目绑定到底层状态
+问题：a `value` prop to a form field without an `onChange` handler. This will render a read-only field. If the field should be mutable use `defaultValue`. Otherwise, set either `onChange` or `readOnly`.
+如果在文本框中输入内容，会发现什么也不会发生。
+我们需要响应修改，然后相应地设置状态。
--- src/PesonalDetails.tsx
+++ src/PesonalDetails.tsx
@@ -9,6 +9,37 @@ interface IProps {
 function PersonalDetails(props: IProps) { //遵守约定，属性由父组件传入，状态由自己管理
     const defaultState: Readonly<IPersonState> = props.DefaultState;
     const [status, setStatus] = useState(props.DefaultState)
+    const updateBinding = (event: any) => {
+        switch (event.target.id) {
+            case `firstName`:
+                setStatus({ ...status, FirstName: event.target.value });
+                break;
+            case `lastName`:
+                setStatus({ ...status, LastName: event.target.value });
+                break;
+            case `addr1`:
+                setStatus({ ...status, Address1: event.target.value });
+                break;
+            case `addr2`:
+                setStatus({ ...status, Address2: event.target.value });
+                break;
+            case `town`:
+                setStatus({ ...status, Town: event.target.value });
+                break;
+            case `county`:
+                setStatus({ ...status, County: event.target.value });
+                break;
+            case `postcode`:
+                setStatus({ ...status, Postcode: event.target.value });
+                break;
+            case `phoneNumber`:
+                setStatus({ ...status, PhoneNumber: event.target.value });
+                break;
+            case `dateOfBirth`:
+                setStatus({ ...status, DateOfBirth: event.target.value });
+                break;
+        }
+    }
     return (
         <Row>
             <Col lg="8">
@@ -27,9 +58,9 @@ function PersonalDetails(props: IProps) { //遵守约定，属性由父组件传
                     <Col>
                         <Row>
                             <Col>
-                                <input type="text" id="firstName" className="form-control" placeholder="First name" value={status.FirstName} />
+                                <input type="text" id="firstName" className="form-control" placeholder="First name" value={status.FirstName} onChange={updateBinding} />
                             </Col>
-                            <Col><input type="text" id="lastName" className="form-control" placeholder="Last name" value={status.LastName} /></Col>
+                            <Col><input type="text" id="lastName" className="form-control" placeholder="Last name" value={status.LastName} onChange={updateBinding} /></Col>
                         </Row>
                     </Col>
                 </Row>
@@ -37,25 +68,25 @@ function PersonalDetails(props: IProps) { //遵守约定，属性由父组件传
                     <Col><label htmlFor="addr1">Address line 1</label></Col>
                 </Row>
                 <Row>
-                    <Col><input type="text" id="addr1" className="form-control" placeholder="Address line 1" value={status.Address1} /></Col>
+                    <Col><input type="text" id="addr1" className="form-control" placeholder="Address line 1" value={status.Address1} onChange={updateBinding} /></Col>
                 </Row>
                 <Row>
                     <Col><label htmlFor="addr2">Address line 2</label></Col>
                 </Row>
                 <Row>
-                    <Col><input type="text" id="addr2" className="form-control" placeholder="Address line 2" value={status.Address2 ? status.Address2 : ""} /></Col>
+                    <Col><input type="text" id="addr2" className="form-control" placeholder="Address line 2" value={status.Address2 == null ? "" : status.Address2} onChange={updateBinding} /></Col>
                 </Row>
                 <Row>
                     <Col><label htmlFor="town">Town</label></Col>
                 </Row>
                 <Row>
-                    <Col><input type="text" id="town" className="form-control" placeholder="Town" value={status.Town} /></Col>
+                    <Col><input type="text" id="town" className="form-control" placeholder="Town" value={status.Town} onChange={updateBinding} /></Col>
                 </Row>
                 <Row>
                     <Col><label htmlFor="county">County</label></Col>
                 </Row>
                 <Row>
-                    <Col><input type="text" id="county" className="form-control" placeholder="County" value={status.County} /></Col>
+                    <Col><input type="text" id="county" className="form-control" placeholder="County" value={status.County} onChange={updateBinding} /></Col>
                 </Row>
                 <Row>
                     <Col>
@@ -68,8 +99,8 @@ function PersonalDetails(props: IProps) { //遵守约定，属性由父组件传
                 <Row>
                     <Col>
                         <Row>
-                            <Col><input type="text" id="postcode" className="form-control" value={status.Postcode} /></Col>
-                            <Col><input type="text" id="phoneNumber" className="form-control" value={status.PhoneNumber} /></Col>
+                            <Col><input type="text" id="postcode" className="form-control" value={status.Postcode} onChange={updateBinding} /></Col>
+                            <Col><input type="text" id="phoneNumber" className="form-control" value={status.PhoneNumber} onChange={updateBinding} /></Col>
                         </Row>
                     </Col>
                 </Row>
@@ -77,7 +108,7 @@ function PersonalDetails(props: IProps) { //遵守约定，属性由父组件传
                     <Col><label htmlFor="dateOfBirth">Date of birth</label></Col>
                 </Row>
                 <Row>
-                    <Col><input type="date" id="dateOfBirth" value={status.DateOfBirth!} /></Col>
+                    <Col><input type="date" id="dateOfBirth" value={status.DateOfBirth!} onChange={updateBinding} /></Col>
                 </Row>
                 <Row>
                     <Col>
@@ -100,5 +131,4 @@ function PersonalDetails(props: IProps) { //遵守约定，属性由父组件传
         </Row>
     );
 }
-
 export default PersonalDetails;
